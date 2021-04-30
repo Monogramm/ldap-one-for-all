9f1b8f5a89ad8d2aa84b016ba783c1966c432944
Nathan-Al <allan4@outlook.fr>
2021-04-30 20:16:14 +0000

    🎨 BuildLdapConfig update
    add use InvalidArgumentException

--- app/src/Command/BuildLdapConfig.php
+++ app/src/Command/BuildLdapConfig.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 
 trait BuildLdapConfig
 {
