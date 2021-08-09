8acf45ee0a9c414eccb27458a3b6b22c46207810
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-08-09 18:17:04 +0200

    ✅ Fix regex for password generation
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- app/tests/Service/PasswordGeneratorUnitTest.php
+++ app/tests/Service/PasswordGeneratorUnitTest.php
@@ -11,7 +11,7 @@ class PasswordGeneratorUnitTest extends TestCase
     {
         $code = (new PasswordGenerator())->generate(8);
         $this->assertIsString($code);
-        $this->assertMatchesRegularExpression('/[A-Z0-9]|[+\-*@&#]/m', $code);
+        $this->assertMatchesRegularExpression('/[A-Za-z0-9]|[+\-*@&#]/m', $code);
         $this->assertEquals(8, strlen($code));
     }
 }
